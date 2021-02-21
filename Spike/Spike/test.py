db6bd6ccac7a547d6a320b06f2f2dd3e6bd2a445
Junyu08 <jwang2289us@126.com>
2021-02-21 19:31:54 +0800

    add order history

--- Spike/Spike/test.py
+++ Spike/Spike/test.py
@@ -2,3 +2,7 @@
 #
 # order = Order("12345", "JunyuWang", "2019-01-02", "", "", "Incomplete")
 # order.insertOrder()
+from Spike.OrderHistoryModel import OrderHistory
+
+order = OrderHistory("12345", "14485", "1", "2021", "2", "12")
+order.insertOrderContent()
