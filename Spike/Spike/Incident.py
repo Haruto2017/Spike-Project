42dcd3acafe8f591f80be3708ae0596c5dcf1f68
Junyu08 <jwang2289us@126.com>
2021-03-25 20:25:58 +0800

    finish five function

--- Spike/Spike/Incident.py
+++ Spike/Spike/Incident.py
@@ -1,16 +1,77 @@
 from Spike import app, mongo
 
+
 class Incident:
-    def __init__(self, IncidentID, State, Location, Date, Description):
+    def __init__(self, IncidentID, State, Location, Year, Month, Day, Description, Category, steerName):
         self.IncidentID = IncidentID
         self.State = State
         self.Location = Location
-        self.Date = Date
+        self.steerName = steerName
+        self.Year = Year
+        self.Month = Month
+        self.Day = Day
         self.Description = Description
+        self.Category = Category
 
-    def insertIncident(self):
-        order_list = mongo.db.Incident
-        order_list.insert_one(
-            {'IncidentID': self.IncidentID, 'State': self.State, 'Location': self.Location,
-             'Date': self.Date,
-             'Description': self.Description})
+
+def InsertIncident(IncidentID, State, Location, Description, Category, Year, Month, Day, steerName):
+    order_list = mongo.db.Incident
+    if findIfIncidentExist(IncidentID) is True:
+        return {"Status": False, "Msg": "Incident Has Existed"}
+    order_list.insert_one(
+        {'IncidentID': IncidentID, 'State': State, 'Location': Location,
+         'Description': Description,
+         'Category': Category,
+         'Year': Year,
+         'Month': Month,
+         'Day': Day,
+         'steerName': steerName
+         })
+    return {"Status": True, "Msg": "Success"}
+
+def findIfIncidentExist(IncidentID):
+    order_list = mongo.db.Incident
+    myquery = {"IncidentID": IncidentID}
+    users = order_list.find(myquery)
+    length = users.count()
+    if length > 0:
+        return True
+    return False
+
+
+def DeleteIncidentById(IncidentID):
+    order_list = mongo.db.Incident
+    myquery = {"IncidentID": IncidentID}
+    if findIfIncidentExist(IncidentID) is False:
+        return {"Status": False, "Msg": "Incident Does Not Exist"}
+    order_list.delete_one(myquery)
+    result = {"Status": True, "Msg": "Success"}
+    return result
+
+
+def FindByCategory(Category):
+    order_list = mongo.db.Incident
+    myquery = {"Category": Category}
+    res = order_list.find(myquery)
+    result = []
+    for x in res:
+        result.append(x)
+    return result
+
+def FindReportsByDate(Year, Month, Day):
+    order_list = mongo.db.Incident
+    myquery = {"Year": Year, "Month": Month, "Day": Day}
+    res = order_list.find(myquery)
+    result = []
+    for x in res:
+        result.append(x)
+    return result
+
+def FindReportsByStreet(steerName):
+    order_list = mongo.db.Incident
+    myquery = {"steerName": steerName}
+    res = order_list.find(myquery)
+    result = []
+    for x in res:
+        result.append(x)
+    return result
--- Spike/Spike/views.py
+++ Spike/Spike/views.py
@@ -2,9 +2,9 @@
 Routes and views for the flask application.
 """
 
-from datetime import datetime
 from flask import render_template, jsonify, request
 from Spike import app, mongo
+from Spike.Incident import InsertIncident, DeleteIncidentById, FindByCategory, FindReportsByDate
 
 
 # @app.route('/')
@@ -17,4 +17,93 @@ from Spike import app, mongo
 #         year=datetime.now().year,
 #     )
 #
+# IncidentID, State, Location, Date, Description, Category
+@app.route('/reportCreate', methods=('GET', 'POST'))
+def report_create():
+    req = request.get_json()
+    result = {"Status": False, "Msg": "Report is lack of information"}
+    IncidentID = req.get("IncidentID")
+    if IncidentID is None:
+        return jsonify(result)
+
+    State = req.get("State")
+    if State is None:
+        return jsonify(result)
+    steerName = req.get("steerName")
+    if steerName is None:
+        return jsonify(result)
+    Location = req.get("Location")
+    if Location is None:
+        return jsonify(result)
+    Year = req.get("Year")
+    if Year is None:
+        return jsonify(result)
+    Month = req.get("Month")
+    if Month is None:
+        return jsonify(result)
+    Day = req.get("Day")
+    if Day is None:
+        return jsonify(result)
+    Description = req.get("Description")
+    if Description is None:
+        return jsonify(result)
+    Category = req.get("Description")
+    if Category is None:
+        return jsonify(result)
+    result = InsertIncident(IncidentID, State, Location, Description, Category, Year, Month, Day, steerName)
+    return jsonify(result)
+
+
+@app.route('/deleteReportByID', methods=('GET', 'POST'))
+def delete():
+    req = request.get_json()
+    result = {"Status": False, "Msg": "Need a IncidentId to delete"}
+    IncidentID = req.get("IncidentID")
+    if IncidentID is None:
+        return jsonify(result)
+    result = DeleteIncidentById(IncidentID)
+    return jsonify(result)
+
+
+@app.route('/getReportByCategory', methods=('GET', 'POST'))
+def getReportByCategory():
+    req = request.get_json()
+    result = {"Status": False, "Msg": "Need a Category to search"}
+    Category = req.get("Category")
+    if Category is None:
+        return jsonify(result)
+    result = FindByCategory(Category)
+    return jsonify(result)
+
+
+@app.route('/getReportByDate', methods=('GET', 'POST'))
+def getReportByDate():
+    result = {"Status": False, "Msg": "Date is lack of information"}
+    req = request.get_json()
+    Year = req.get("Year")
+    if Year is None:
+        return jsonify(result)
+    Month = req.get("Month")
+    if Month is None:
+        return jsonify(result)
+    Day = req.get("Day")
+    if Day is None:
+        return jsonify(result)
+    FindReportsByDate(Year, Month, Day)
+    result = {"Status": True, "Msg": "Success"}
+    return jsonify(result)
+
+
+@app.route('/getReportByLocation', methods=('GET', 'POST'))
+def getReportByLocation():
+    result = {"Status": False, "Msg": "Date is lack of information"}
+    req = request.get_json()
+    steerName = req.get("steerName")
+    if steerName is None:
+        return jsonify(result)
+
+    FindReportsByDate(steerName)
+    result = {"Status": True, "Msg": "Success"}
+    return jsonify(result)
+
 
